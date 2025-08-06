b682f6460828f1473dcb84a564b9afc1094bdb5f
HC <hemachand2508@gmail.com>
2025-08-06 10:35:18 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'jenkinsrep' // deployment folder but not repo name
+  base:'jenkinserp' // deployment folder but not repo name
 })
